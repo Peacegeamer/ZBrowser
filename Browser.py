340ff498fe392cfd47728e0d7b13a550d9029663
MayonnaiseSoup <93639973+MayonnaiseSoup@users.noreply.github.com>
2023-09-01 17:12:51 +0800

    Substantial updates, allowing for users to add their own definitions to a database

--- Browser.py
+++ Browser.py
@@ -1,27 +1,63 @@
-print("Hello User!")
-while True:
-    Name = input("Please input your name: ")
-    b = input(f"you will be called {Name}. Y/N: ")
-    if b == "Y":
-        break
-        print("Horray you've started your incognito signup.")
-    elif b == "y":
-        print("Horray you've started your incognito signup.")
-        break
-    elif b == "N":
-        continue
+# Import required modules
+import os
+import psycopg2
 
+dbPassword = os.getenv("DBPASS")
+
+# Connection Data
+conn = psycopg2.connect(database="postgres",
+                        user="postgres",
+                        host="localhost",
+                        password="insert your password here!",
+                        port=5432)
+
+cur = conn.cursor()
+
+os.system("clear")
+
+# Greet the user
+print("Hello User!")
+
+# Ask the user what they want to search!
 while True:
-    Search = input("search query: ")
-    if Search == "helpme" or Search == "help":
-        print("To exit the browser press `ctrl c`")
-        print("Here is a list is of search queries that you can use;")
-        print("helpme or help; These search queries open up a help menu (the one you are on right now). They are probably the most important queries on the browser.")
-        print("beans; This search query was added by MayonaiseSoup (github).")
-        continue
-    if Search == "beans":
-        print("Beans are tasty :D")
-        continue
-    if Search == "goofy":
-        print("Goofy is a character of walt disney, it is also a word used when someone did somthing that is crazy/silly. also used in 'That was goofy ahh'.")
-        continue
+
+    print("Please choose an option: ")
+    print("1. Make a definition")
+    print("2. Search for a definition")
+
+    option = input("\nEnter a number: ")
+
+    if option == "1":
+        os.system("clear")
+        
+        word = input("Which word do you want to add: ")
+
+        word = word.lower()
+
+        meaning = input("What is the meaning of the word: ")
+
+        cur.execute(f"insert into definitions (word, meaning) values ('{word}', '{meaning}')")
+        conn.commit()
+
+        os.system("clear")
+        print("Successfully added the meaning to CouscousDB")
+
+    if option == "2":
+        os.system("clear")
+        word = input("Which word are you looking for? ")
+        word = word.lower()
+
+        cur.execute("select * from definitions")
+
+        rows = cur.fetchall()
+
+        conn.commit()
+
+        os.system("clear")
+
+        for row in rows:
+            if row[0] == word:
+                print(f"{word}:")
+                print(f"{row[1]}\n")
+
+        
